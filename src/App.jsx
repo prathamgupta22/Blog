55282bb07515f3106cd4e720dcd5726713f92e6f
prathamgupta22 <pratham.guptapg01@gmail.com>
2023-09-26 14:04:13 +0530

    started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <h1>this is blog website</h1>
+    </>
+  );
+}
+
+export default App;
